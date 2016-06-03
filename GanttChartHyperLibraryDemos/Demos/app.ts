67523bc85d6d00829857ffb295be84f10e2e4fd1
DlhSoft Team <Contact@DlhSoft.com>
2016-06-03 12:28:43 +0300

    Blue theme rename to Generic blue

--- GanttChartHyperLibraryDemos/Demos/app.ts
+++ GanttChartHyperLibraryDemos/Demos/app.ts
@@ -1,7 +1,7 @@
 ﻿declare var angular;
 angular.module('Demos', [])
     .controller('MainController', ($scope, $http, $timeout) => {
-        var themes = ['Blue', 'Default'];
+        var themes = ['Generic blue', 'Default'];
         $scope.themes = themes;
         $scope.selectedTheme = themes[0];
         $scope.selectTheme = (theme) => {
