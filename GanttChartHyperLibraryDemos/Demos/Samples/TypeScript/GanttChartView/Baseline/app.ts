8a4b9ce6f8084200aef8c0fcc67b744745b15576
DlhSoft <Contact@DlhSoft.com>
2016-07-03 14:48:29 +0300

    More TypeScript samples

---
+++ GanttChartHyperLibraryDemos/Demos/Samples/TypeScript/GanttChartView/Baseline/app.ts
@@ -0,0 +1,55 @@
+﻿/// <reference path='./Scripts/DlhSoft.ProjectData.GanttChart.HTML.Controls.d.ts'/>
+import GanttChartView = DlhSoft.Controls.GanttChartView;
+import GanttChartItem = GanttChartView.Item;
+import PredecessorItem = GanttChartView.PredecessorItem;
+
+// Query string syntax: ?theme
+// Supported themes: Generic-blue, Default.
+var queryString = window.location.search;
+var theme = queryString ? queryString.substr(1) : null;
+declare var initializeGanttChartTemplates;
+declare var initializeGanttChartTheme;
+
+// Retrieve and store the control element for reference purposes.
+var ganttChartViewElement = <HTMLElement>document.querySelector('#ganttChartView');
+
+var date = new Date(), year = date.getFullYear(), month = date.getMonth(), dayDuration = 24 * 60 * 60 * 1000;
+var items = <GanttChartItem[]>[
+    { content: 'Task 1', isExpanded: false, start: new Date() },
+    { content: 'Task 1.1', indentation: 1, start: new Date(year, month, 2, 8, 0, 0), finish: new Date(year, month, 4, 16, 0, 0) },
+    { content: 'Task 1.2', indentation: 1, start: new Date(year, month, 3, 8, 0, 0), finish: new Date(year, month, 5, 12, 0, 0) },
+    { content: 'Task 2', isExpanded: true, start: new Date() },
+    { content: 'Task 2.1', indentation: 1, start: new Date(year, month, 2, 8, 0, 0), finish: new Date(year, month, 8, 16, 0, 0), completedFinish: new Date(year, month, 5, 16, 0, 0), assignmentsContent: 'Resource 1, Resource 2 [50%]' },
+    { content: 'Task 2.2', indentation: 1, start: new Date() },
+    { content: 'Task 2.2.1', indentation: 2, start: new Date(year, month, 11, 8, 0, 0), finish: new Date(year, month, 12, 16, 0, 0), completedFinish: new Date(year, month, 12, 16, 0, 0), assignmentsContent: 'Resource 2' },
+    { content: 'Task 2.2.2', indentation: 2, start: new Date(year, month, 12, 12, 0, 0), finish: new Date(year, month, 14, 16, 0, 0), assignmentsContent: 'Resource 2' },
+    { content: 'Task 2.3', indentation: 1, start: new Date(year, month, 15, 16, 0, 0), isMilestone: true }];
+items[3].predecessors = [{ item: items[0], dependencyType: 'SS' }];
+items[7].predecessors = [{ item: items[6], lag: 2 * 60 * 60 * 1000 }];
+items[8].predecessors = [{ item: items[4] }, { item: items[5] }];
+for (var i = 3; i <= 20; i++)
+    items.push({ content: 'Task ' + i, start: new Date(year, month, 2, 8, 0, 0), finish: new Date(year, month, 4, 16, 0, 0) });
+
+for (var i = 0; i < items.length; i++) {
+    var item = items[i];
+    if (typeof item.start === "undefined" || typeof item.finish === "undefined")
+        continue;
+    item.baselineStart = new Date(item.start.valueOf() - dayDuration);
+    item.baselineFinish = new Date(item.finish.valueOf() - dayDuration);
+}
+items[6].baselineStart = new Date(year, month, 10, 8, 0, 0);
+items[6].baselineFinish = new Date(year, month, 11, 16, 0, 0);
+items[7].baselineStart = new Date(year, month, 8, 8, 0, 0);
+items[7].baselineFinish = new Date(year, month, 11, 16, 0, 0);
+items[8].baselineStart = new Date(year, month, 12, 8, 0, 0);
+
+var settings = <GanttChartView.Settings>{ currentTime: new Date(year, month, 2, 12, 0, 0) };
+
+// Optionally, initialize custom theme and templates (themes.js, templates.js).
+if (initializeGanttChartTheme)
+    initializeGanttChartTheme(settings, theme);
+if (initializeGanttChartTemplates)
+    initializeGanttChartTemplates(settings, theme);
+
+// Initialize the component.
+var ganttChartView = DlhSoft.Controls.GanttChartView.initialize(ganttChartViewElement, items, settings);
