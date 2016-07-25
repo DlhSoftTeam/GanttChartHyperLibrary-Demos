f46763d2d457d8839393945888d2918bef0a394c
DlhSoft Team <Contact@DlhSoft.com>
2016-07-25 16:31:54 +0300

    Miscellaneous updates

--- GanttChartHyperLibraryDemos/Demos/Samples/JavaScript/GanttChartView/ExportPngImage-PMF/app.js
+++ GanttChartHyperLibraryDemos/Demos/Samples/JavaScript/GanttChartView/ExportPngImage-PMF/app.js
@@ -15,7 +15,7 @@ var items = [
     { content: 'Task 2', isExpanded: true, start: new Date() },
     { content: 'Task 2.1', indentation: 1, start: new Date(year, month, 2, 8, 0, 0), finish: new Date(year, month, 8, 16, 0, 0), completedFinish: new Date(year, month, 5, 16, 0, 0), assignmentsContent: 'Resource 1, Resource 2 [50%]' },
     { content: 'Task 2.2', indentation: 1, start: new Date() },
-    { content: 'Task 2.2.1', indentation: 2, start: new Date(year, month, 11, 8, 0, 0), finish: new Date(year, month, 12, 16, 0, 0), completedFinish: new Date(year, month, 12, 16, 0, 0), assignmentsContent: 'Resource 2' },
+    { content: 'Task 2.2.1', indentation: 2, start: new Date(year, month, 11, 8, 0, 0), finish: new Date(year, month, 14, 16, 0, 0), completedFinish: new Date(year, month, 14, 16, 0, 0), assignmentsContent: 'Resource 2' },
     { content: 'Task 2.2.2', indentation: 2, start: new Date(year, month, 12, 12, 0, 0), finish: new Date(year, month, 14, 16, 0, 0), assignmentsContent: 'Resource 2' },
     { content: 'Task 3', indentation: 1, start: new Date(year, month, 14, 16, 0, 0), isMilestone: true }];
 items[3].predecessors = [{ item: items[0], dependencyType: 'SS' }];
