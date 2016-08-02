dc49900a891599dc4913927bdd3ff1ec84d6b5d2
DlhSoft <Contact@DlhSoft.com>
2016-08-02 13:52:40 +0300

    Basic usage samples for AngularJS

---
+++ GanttChartHyperLibraryDemos/Demos/Samples/AngularJS/LoadChartView/SingleItem/app.ts
@@ -0,0 +1,37 @@
+﻿/// <reference path='./Scripts/DlhSoft.ProjectData.GanttChart.HTML.Controls.d.ts'/>
+import LoadChartView = DlhSoft.Controls.LoadChartView;
+import LoadChartItem = LoadChartView.Item;
+import AllocationItem = LoadChartView.AllocationItem;
+
+// Query string syntax: ?theme
+// Supported themes: Default, Generic-bright, Generic-blue, DlhSoft-gray, Purple-green, Steel-blue, Dark-black, Cyan-green, Blue-navy, Orange-brown, Teal-green, Purple-beige, Gray-blue, Aero.
+var queryString = window.location.search;
+var theme = queryString ? queryString.substr(1) : null;
+declare var initializeLoadChartTheme;
+
+declare var angular;
+angular.module('LoadChartViewSample', ['DlhSoft.ProjectData.GanttChart.Directives'])
+    .controller('MainController', ($scope, $http, $timeout) => {
+        // Prepare a single data item with multiple allocations.
+        var loadChartItems = <LoadChartItem[]>[{
+            content: 'Resource 1', start: new Date(), ganttChartItems: [
+                { content: 'Task 1 (Resource 1)', start: new Date(2016, 2 - 1, 2, 8, 0, 0), finish: new Date(2016, 2 - 1, 2, 16, 0, 0) },
+                { content: 'Task 1, Task 2 [50%] (Resource 1): 150%', start: new Date(2016, 2 - 1, 3, 8, 0, 0), finish: new Date(2016, 2 - 1, 3, 12, 0, 0), units: 1.5 },
+                { content: 'Task 2 [50%] (Resource 1)', start: new Date(2016, 2 - 1, 3, 12, 0, 0), finish: new Date(2016, 2 - 1, 4, 16, 0, 0), units: 0.5 },
+                { content: 'Task 3 (Resource 1)', start: new Date(2016, 2 - 1, 6, 8, 0, 0), finish: new Date(2016, 2 - 1, 6, 16, 0, 0) }]
+        }];
+        $scope.items = loadChartItems;
+        var settings = <LoadChartView.Settings>{
+            currentTime: new Date(2016, 2 - 1, 2) // Display the current time vertical line of the chart at the project start date.
+        };
+        // Optionally, initialize custom theme (themes.js).
+        if (initializeLoadChartTheme)
+            initializeLoadChartTheme(settings, theme);
+        // settings.isGridVisible = false;
+        settings.itemHeight = 312;
+        settings.barMargin = 8;
+        settings.barHeight = settings.itemHeight - settings.barMargin * 2;
+        $scope.settings = settings;
+        // Underlying LoadChartView component reference.
+        var loadChartView = <LoadChartView.Element>document.getElementById('loadChartView');
+    });
---
+++ GanttChartHyperLibraryDemos/Demos/Samples/AngularJS/PertChartView/BasicUsage/app.js
@@ -0,0 +1,32 @@
+/// <reference path='./Scripts/DlhSoft.ProjectData.PertChart.HTML.Controls.d.ts'/>
+var PertChartView = DlhSoft.Controls.Pert.PertChartView;
+// Query string syntax: ?theme
+// Supported themes: Default, Generic-bright, Generic-blue, DlhSoft-gray, Purple-green, Steel-blue, Dark-black, Cyan-green, Blue-navy, Orange-brown, Teal-green, Purple-beige, Gray-blue, Aero.
+var queryString = window.location.search;
+var theme = queryString ? queryString.substr(1) : null;
+angular.module('PertChartViewSample', ['DlhSoft.ProjectData.PertChart.Directives'])
+    .controller('MainController', function ($scope, $http, $timeout) {
+    // Prepare PERT Chart data items and settings.
+    var secondDuration = 1000, minuteDuration = 60 * secondDuration, hourDuration = 60 * minuteDuration;
+    var items = [
+        { displayedText: '0', content: 'Start' },
+        { displayedText: '1', content: 'Task event 1' },
+        { displayedText: '2', content: 'Task event 2' },
+        { displayedText: '3', content: 'Task event 3' },
+        { displayedText: '4', content: 'Finish', displayedRowIndex: 0 }];
+    items[1].predecessors = [{ item: items[0], displayedText: 'A', content: 'Task A', effort: 4 * hourDuration }];
+    items[2].predecessors = [{ item: items[0], displayedText: 'B', content: 'Task B', effort: 2 * hourDuration }];
+    items[3].predecessors = [{ item: items[2], displayedText: 'C', content: 'Task C', effort: 1 * hourDuration }];
+    items[4].predecessors = [
+        { item: items[1], displayedText: 'D', content: 'Task D', effort: 5 * hourDuration },
+        { item: items[2], displayedText: 'E', content: 'Task E', effort: 3 * hourDuration },
+        { item: items[3], displayedText: 'F', content: 'Task F', effort: 2 * hourDuration }];
+    $scope.items = items;
+    var settings = {};
+    // Optionally, initialize custom theme (themes.js).
+    if (initializePertChartTheme)
+        initializePertChartTheme(settings, theme);
+    $scope.settings = settings;
+    // Underlying PertChartView component reference.
+    var pertChartView = document.getElementById('pertChartView');
+});
