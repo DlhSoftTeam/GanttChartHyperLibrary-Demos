6815f1141b74f5a3e266809a41c00e27d9c67169
DlhSoft <Contact@DlhSoft.com>
2016-10-21 16:48:08 +0300

    More ScheduleChartView samples

---
+++ GanttChartHyperLibraryDemos/Demos/Samples/JavaScript/ScheduleChartView/ReadOnlySettings/app.js
@@ -0,0 +1,69 @@
+/// <reference path='./Scripts/DlhSoft.ProjectData.GanttChart.HTML.Controls.d.ts'/>
+var GanttChartView = DlhSoft.Controls.GanttChartView;
+var ScheduleChartView = DlhSoft.Controls.ScheduleChartView;
+// Query string syntax: ?theme
+// Supported themes: Default, Generic-bright, Generic-blue, DlhSoft-gray, Purple-green, Steel-blue, Dark-black, Cyan-green, Blue-navy, Orange-brown, Teal-green, Purple-beige, Gray-blue, Aero.
+var queryString = window.location.search;
+var theme = queryString ? queryString.substr(1) : null;
+// Retrieve and store the control element for reference purposes.
+var scheduleChartViewElement = document.querySelector('#scheduleChartView');
+var date = new Date(), year = date.getFullYear(), month = date.getMonth();
+var scheduleChartItems = [
+    { content: 'Resource 1', start: new Date(), ganttChartItems: [{ content: 'Task A (Resource 1)', start: new Date(year, month, 2, 8, 0, 0), finish: new Date(year, month, 8, 16, 0, 0), completedFinish: new Date(year, month, 5, 16, 0, 0) }] },
+    {
+        content: 'Resource 2', start: new Date(), ganttChartItems: [
+            { content: 'Task A (Resource 2)', start: new Date(year, month, 2, 8, 0, 0), finish: new Date(year, month, 8, 16, 0, 0), completedFinish: new Date(year, month, 5, 16, 0, 0), assignmentsContent: '50%' },
+            { content: 'Task B (Resource 2)', start: new Date(year, month, 11, 8, 0, 0), finish: new Date(year, month, 12, 16, 0, 0), completedFinish: new Date(year, month, 12, 16, 0, 0) },
+            { content: 'Task C (Resource 2)', start: new Date(year, month, 14, 8, 0, 0), finish: new Date(year, month, 14, 16, 0, 0) }]
+    },
+    { content: 'Resource 3', start: new Date(), ganttChartItems: [{ content: 'Task D (Resource 3)', start: new Date(year, month, 12, 12, 0, 0), finish: new Date(year, month, 14, 16, 0, 0) }] }];
+for (var i = 4; i <= 16; i++)
+    scheduleChartItems.push({
+        content: 'Resource ' + i, start: new Date(), ganttChartItems: [
+            { content: 'Task X (Resource ' + i + ')', start: new Date(year, month, 2, 8, 0, 0), finish: new Date(year, month, 5, 16, 0, 0) },
+            { content: 'Task Y (Resource ' + i + ')', start: new Date(year, month, 7, 8, 0, 0), finish: new Date(year, month, 8, 16, 0, 0) }]
+    });
+var scheduleChartView;
+var settings;
+// Prepare command element references.
+var isReadOnlyCheckBox = document.querySelector('#isReadOnlyCheckBox');
+var isGridReadOnlyCheckBox = document.querySelector('#isGridReadOnlyCheckBox');
+var isChartReadOnlyCheckBox = document.querySelector('#isChartReadOnlyCheckBox');
+var isStartReadOnlyCheckBox = document.querySelector('#isStartReadOnlyCheckBox');
+var isEffortReadOnlyCheckBox = document.querySelector('#isEffortReadOnlyCheckBox');
+var isCompletionReadOnlyCheckBox = document.querySelector('#isCompletionReadOnlyCheckBox');
+var disableStartEndDraggingCheckBox = document.querySelector('#disableStartEndDraggingCheckBox');
+var disableScrollingOnTaskClick = document.querySelector('#disableScrollingOnTaskClick');
+var hideGridCheckBox = document.querySelector('#hideGridCheckBox');
+// Initialize settings based on user selections.
+function initialize() {
+    settings = { currentTime: new Date(year, month, 2, 12, 0, 0) };
+    settings.isReadOnly = isReadOnlyCheckBox.checked;
+    settings.isGridReadOnly = isGridReadOnlyCheckBox.checked;
+    settings.isChartReadOnly = isChartReadOnlyCheckBox.checked;
+    settings.isGridVisible = !hideGridCheckBox.checked;
+    settings.isTaskStartReadOnly = isStartReadOnlyCheckBox.checked;
+    settings.isTaskEffortReadOnly = isEffortReadOnlyCheckBox.checked;
+    settings.isTaskCompletionReadOnly = isCompletionReadOnlyCheckBox.checked;
+    settings.areAssignmentsReadOnly = false; // Doesn't support updates upon reinitialization (unless you clean up the internal draggable dependency cache using: delete scheduleChartViewElement['draggableAssignmentItems']).
+    settings.isDraggingTaskStartEndsEnabled = !disableStartEndDraggingCheckBox.checked;
+    settings.isGridRowClickTimeScrollingEnabled = !disableScrollingOnTaskClick.checked;
+    // Optionally, initialize custom theme and templates (themes.js, templates.js).
+    if (initializeGanttChartTheme)
+        initializeGanttChartTheme(settings, theme);
+    if (initializeGanttChartTemplates)
+        initializeGanttChartTemplates(settings, theme);
+    // (Re-)Initialize the component.
+    scheduleChartView = DlhSoft.Controls.ScheduleChartView.initialize(scheduleChartViewElement, scheduleChartItems, settings);
+}
+initialize();
+// Reinitialize settings and the component upon user commands.
+isReadOnlyCheckBox.addEventListener('change', initialize);
+isGridReadOnlyCheckBox.addEventListener('change', initialize);
+isChartReadOnlyCheckBox.addEventListener('change', initialize);
+hideGridCheckBox.addEventListener('change', initialize);
+isStartReadOnlyCheckBox.addEventListener('change', initialize);
+isEffortReadOnlyCheckBox.addEventListener('change', initialize);
+isCompletionReadOnlyCheckBox.addEventListener('change', initialize);
+disableStartEndDraggingCheckBox.addEventListener('change', initialize);
+disableScrollingOnTaskClick.addEventListener('change', initialize);
