112255961bf7b8667ffd702a124f72258a38f531
DlhSoft <Contact@DlhSoft.com>
2016-10-21 12:22:43 +0300

    More ScheduleChartView and LoadChartView samples

---
+++ GanttChartHyperLibraryDemos/Demos/Samples/TypeScript/ScheduleChartView/Printing/app.js
@@ -0,0 +1,32 @@
+var GanttChartView = DlhSoft.Controls.GanttChartView;
+var ScheduleChartView = DlhSoft.Controls.ScheduleChartView;
+var queryString = window.location.search;
+var theme = queryString ? queryString.substr(1) : null;
+var scheduleChartViewElement = document.querySelector('#scheduleChartView');
+var date = new Date(), year = date.getFullYear(), month = date.getMonth();
+var scheduleChartItems = [
+    { content: 'Resource 1', start: new Date(), ganttChartItems: [{ content: 'Task A (Resource 1)', start: new Date(year, month, 2, 8, 0, 0), finish: new Date(year, month, 8, 16, 0, 0), completedFinish: new Date(year, month, 5, 16, 0, 0) }] },
+    {
+        content: 'Resource 2', start: new Date(), ganttChartItems: [
+            { content: 'Task A (Resource 2)', start: new Date(year, month, 2, 8, 0, 0), finish: new Date(year, month, 8, 16, 0, 0), completedFinish: new Date(year, month, 5, 16, 0, 0), assignmentsContent: '50%' },
+            { content: 'Task B (Resource 2)', start: new Date(year, month, 11, 8, 0, 0), finish: new Date(year, month, 12, 16, 0, 0), completedFinish: new Date(year, month, 12, 16, 0, 0) },
+            { content: 'Task C (Resource 2)', start: new Date(year, month, 14, 8, 0, 0), finish: new Date(year, month, 14, 16, 0, 0) }]
+    },
+    { content: 'Resource 3', start: new Date(), ganttChartItems: [{ content: 'Task D (Resource 3)', start: new Date(year, month, 12, 12, 0, 0), finish: new Date(year, month, 14, 16, 0, 0) }] }];
+for (var i = 4; i <= 16; i++)
+    scheduleChartItems.push({
+        content: 'Resource ' + i, start: new Date(), ganttChartItems: [
+            { content: 'Task X (Resource ' + i + ')', start: new Date(year, month, 2, 8, 0, 0), finish: new Date(year, month, 5, 16, 0, 0) },
+            { content: 'Task Y (Resource ' + i + ')', start: new Date(year, month, 7, 8, 0, 0), finish: new Date(year, month, 8, 16, 0, 0) }]
+    });
+var settings = {
+    currentTime: new Date(year, month, 2, 12, 0, 0)
+};
+if (initializeGanttChartTheme)
+    initializeGanttChartTheme(settings, theme);
+if (initializeGanttChartTemplates)
+    initializeGanttChartTemplates(settings, theme);
+var scheduleChartView = DlhSoft.Controls.ScheduleChartView.initialize(scheduleChartViewElement, scheduleChartItems, settings);
+function print() {
+    scheduleChartView.print({ title: 'Schedule Chart (printable)', isGridVisible: true, columnIndexes: [0], timelineStart: new Date(year, month, 1), timelineFinish: new Date(new Date(year, month, 1).valueOf() + 5 * 7 * 24 * 60 * 60 * 1000), preparingMessage: '...' });
+}
---
+++ GanttChartHyperLibraryDemos/Demos/Samples/TypeScript/ScheduleChartView/ResourceStatus/app.ts
@@ -0,0 +1,65 @@
+﻿/// <reference path='./Scripts/DlhSoft.ProjectData.GanttChart.HTML.Controls.d.ts'/>
+import GanttChartView = DlhSoft.Controls.GanttChartView;
+import GanttChartItem = GanttChartView.Item;
+import ScheduleChartView = DlhSoft.Controls.ScheduleChartView;
+import ScheduleChartItem = ScheduleChartView.Item;
+
+// Query string syntax: ?theme
+// Supported themes: Default, Generic-bright, Generic-blue, DlhSoft-gray, Purple-green, Steel-blue, Dark-black, Cyan-green, Blue-navy, Orange-brown, Teal-green, Purple-beige, Gray-blue, Aero.
+var queryString = window.location.search;
+var theme = queryString ? queryString.substr(1) : null;
+declare var initializeGanttChartTemplates;
+declare var initializeGanttChartTheme;
+
+// Retrieve and store the control element for reference purposes.
+var scheduleChartViewElement = <HTMLElement>document.querySelector('#scheduleChartView');
+
+// Prepare data items with barClass values set to indicate different CSS classes as needed, and content set to display tool tips accordingly.
+var date = new Date(), year = date.getFullYear(), month = date.getMonth();
+var scheduleChartItems = <ScheduleChartItem[]>[
+    {
+        content: 'Resource 1', start: new Date(), ganttChartItems: [
+            { content: 'Resource 1: started', start: new Date(year, month, 2, 8, 0, 0), finish: new Date(year, month, 8, 16, 0, 0), barClass: 'startedStatusBar' },
+            { content: 'Resource 1: maintenance', start: new Date(year, month, 9, 8, 0, 0), finish: new Date(year, month, 9, 16, 0, 0), barClass: 'maintenanceStatusBar' },
+            { content: 'Resource 1: started', start: new Date(year, month, 10, 8, 0, 0), finish: new Date(year, month, 13, 16, 0, 0), barClass: 'startedStatusBar' },
+            { content: 'Resource 1: issues', start: new Date(year, month, 14, 8, 0, 0), finish: new Date(year, month, 14, 16, 0, 0), barClass: 'issuesStatusBar' },
+            { content: 'Resource 1: maintenance', start: new Date(year, month, 15, 8, 0, 0), finish: new Date(year, month, 15, 16, 0, 0), barClass: 'maintenanceStatusBar' },
+            { content: 'Resource 1: started', start: new Date(year, month, 16, 8, 0, 0), finish: new Date(year, month, 22, 16, 0, 0), barClass: 'startedStatusBar' }]
+    },
+    {
+        content: 'Resource 2', start: new Date(), ganttChartItems: [
+            { content: 'Resource 2: started', start: new Date(year, month, 2, 8, 0, 0), finish: new Date(year, month, 8, 16, 0, 0), barClass: 'startedStatusBar' },
+            { content: 'Resource 2: issues', start: new Date(year, month, 9, 8, 0, 0), finish: new Date(year, month, 12, 16, 0, 0), barClass: 'issuesStatusBar' },
+            { content: 'Resource 2: maintenance', start: new Date(year, month, 13, 8, 0, 0), finish: new Date(year, month, 14, 16, 0, 0), barClass: 'maintenanceStatusBar' },
+            { content: 'Resource 2: started', start: new Date(year, month, 15, 8, 0, 0), finish: new Date(year, month, 22, 16, 0, 0), barClass: 'startedStatusBar' }]
+    },
+    {
+        content: 'Resource 3', start: new Date(), ganttChartItems: [
+            { content: 'Resource 3: started', start: new Date(year, month, 2, 8, 0, 0), finish: new Date(year, month, 22, 16, 0, 0), barClass: 'startedStatusBar' }]
+    }];
+for (var i = 4; i <= 16; i++)
+    scheduleChartItems.push({
+        content: 'Resource ' + i, start: new Date(), ganttChartItems: [
+            { content: 'Resource ' + i + ': started', start: new Date(year, month, 2, 8, 0, 0), finish: new Date(year, month, 5, 16, 0, 0), barClass: 'startedStatusBar' },
+            { content: 'Resource ' + i + ': issues', start: new Date(year, month, 6, 8, 0, 0), finish: new Date(year, month, 6, 16, 0, 0), barClass: 'issuesStatusBar' },
+            { content: 'Resource ' + i + ': maintenance', start: new Date(year, month, 7, 8, 0, 0), finish: new Date(year, month, 7, 16, 0, 0), barClass: 'maintenanceStatusBar' },
+            { content: 'Resource ' + i + ': started', start: new Date(year, month, 8, 8, 0, 0), finish: new Date(year, month, 15, 16, 0, 0), barClass: 'startedStatusBar' },
+            { content: 'Resource ' + i + ': issues', start: new Date(year, month, 16, 8, 0, 0), finish: new Date(year, month, 16, 16, 0, 0), barClass: 'issuesStatusBar' },
+            { content: 'Resource ' + i + ': started', start: new Date(year, month, 17, 8, 0, 0), finish: new Date(year, month, 22, 16, 0, 0), barClass: 'startedStatusBar' }]
+    });
+
+// Prepare control settings.
+var settings = <ScheduleChartView.Settings>{
+    isReadOnly: true, isTaskCompletedEffortVisible: false,
+    workingWeekStart: 0, workingWeekFinish: 6,
+    currentTime: new Date(year, month, 2, 12, 0, 0)
+};
+
+// Optionally, initialize custom theme and templates (themes.js, templates.js).
+if (initializeGanttChartTheme)
+    initializeGanttChartTheme(settings, theme);
+if (initializeGanttChartTemplates)
+    initializeGanttChartTemplates(settings, theme);
+
+// Initialize the component.
+var scheduleChartView = DlhSoft.Controls.ScheduleChartView.initialize(scheduleChartViewElement, scheduleChartItems, settings);
