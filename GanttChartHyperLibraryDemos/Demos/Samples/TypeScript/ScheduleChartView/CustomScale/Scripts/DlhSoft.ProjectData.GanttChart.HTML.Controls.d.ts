e47c6237a971043d3258352f5f378212ebc21305
DlhSoft Team <Contact@DlhSoft.com>
2016-11-03 16:10:16 +0200

    Updated DlhSoft packages

--- GanttChartHyperLibraryDemos/Demos/Samples/TypeScript/ScheduleChartView/CustomScale/Scripts/DlhSoft.ProjectData.GanttChart.HTML.Controls.d.ts
+++ GanttChartHyperLibraryDemos/Demos/Samples/TypeScript/ScheduleChartView/CustomScale/Scripts/DlhSoft.ProjectData.GanttChart.HTML.Controls.d.ts
@@ -1,7 +1,7 @@
 ﻿/* Assembly: DlhSoft.ProjectData.GanttChart.HTML.Controls,
    Company: DlhSoft,
    Product: Project Data Modern Library,
-   Version: 5.3.10.1,
+   Version: 5.3.10.2,
    Copyright: Copyright © 2012-2016 DlhSoft,
    Title: Project Data Gantt Chart HTML Controls,
    Description: Project Data Gantt Chart related HTML client components */
@@ -745,6 +745,7 @@ declare module DlhSoft.Controls {
             temporaryAssignmentThumbClass?: string; temporaryAssignmentThumbStyle?: string;
 
             assignmentThumbTemplate? (item: GanttChartView.Item): SVGElement;
+            temporaryAssignmentThumbTemplate? (document: HTMLDocument, x: number, y: number, width: number, height: number): SVGElement;
         }
     }
 
