dba9e3ac724fd6b737830fcfc041e5076f9f45ab
DlhSoft <Contact@DlhSoft.com>
2017-03-29 18:25:22 +0300

    Support for Safari (iPhone)

--- GanttChartHyperLibraryDemos/Demos/app.js
+++ GanttChartHyperLibraryDemos/Demos/app.js
@@ -693,3 +693,18 @@ angular.module('Demos', [])
         templateUrl: 'Templates/SourceCode.html'
     };
 });
+$(document).ready(function () {
+    var body = $(document).find('body');
+    var shouldSyncSize = false;
+    var syncSizeTimer = setInterval(function () {
+        var sampleFrame = $('#sample-frame');
+        var sampleBody = sampleFrame.contents().find('body');
+        var bodyWidth = body.width(), sampleBodyWidth = sampleBody.width();
+        if (sampleBodyWidth > bodyWidth)
+            shouldSyncSize = true;
+        if (shouldSyncSize && sampleBodyWidth != bodyWidth)
+            sampleBody.width(bodyWidth + 'px');
+        if (!shouldSyncSize)
+            clearInterval(syncSizeTimer);
+    }, 500);
+});
